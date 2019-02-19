fed1f0b3f55c52e97ced901eede2d48a28eb4a7d
Cameron <camcauser84@gmail.com>
2019-02-19 08:26:04 -0500

    updated code to add in classes

--- gui.py
+++ gui.py
@@ -3,80 +3,92 @@
 from tkinter import *
 import time
 
+# Class that handles clock functionality
+class Clock:
+    def __init__(self): 
+        # Alarm Strings
+        self.currentTime = ''
+        self.nextTime = ''
+        self.nextAlarmTime = '22:56 pm'
+
+        # Status Strings
+        self.alarmStatusStr = 'Alarm: OFF'
+        self.brewStatusStr =  'Brew:  OFF'
+        self.lightStatusStr = 'Light: OFF'
+
+        # Status Booleans (init all false)
+        self.alarmStatus = True
+        self.brewStatus =  False
+        self.lightStatus = False
+
+    # Fucntion that gets the current time every 200ms and updates appropriate label
+    def tick(self, clockLabel):
+        # global currentTime
+        # get the current local time from the PC
+        self.nextTime = time.strftime('%H:%M pm')
+        # if time string has changed, update it
+        if self.nextTime != self.currentTime:
+            self.currentTime = self.nextTime
+            clockLabel.config(text=self.nextTime)
+        
+        # check to see if an alarm is ready
+        if (self.alarmStatus and self.currentTime == self.nextAlarmTime):
+            self.alarm(clockLabel)
+
+        clockLabel.after(200, self.tick, clockLabel)
+
+    # Function that gets called when the alarm sounds
+    def alarm(self, clockLabel):
+        #sound the alarm
+        clockLabel.config(text = 'ALARM')
+
+    # Callback functions for updating gui status strings
+    def updateAlarmStatus(self, statusLabel):
+        if (self.alarmStatus == False):
+            self.alarmStatusStr = 'Alarm: ON'
+        else:
+            self.alarmStatusStr = 'Alarm: OFF'
+
+        self.alarmStatus = not self.alarmStatus
+        statusLabel.config(text=self.alarmStatusStr + '\n' + self.brewStatusStr + '\n' + self.lightStatusStr)
+
+    def updateBrewStatus(self, statusLabel):
+        if (self.brewStatus == False):
+            self.brewStatusStr =  'Brew:  ON'
+        else:
+            self.brewStatusStr =  'Brew:  OFF'
+
+        self.brewStatus = not self.brewStatus    
+        statusLabel.config(text=self.alarmStatusStr + '\n' + self.brewStatusStr + '\n' + self.lightStatusStr)
+
+    def updateLightStatus(self, statusLabel):
+        if (self.lightStatus == False):
+            self.lightStatusStr = 'Light: ON'
+        else:
+            self.lightStatusStr = 'Light: OFF'
+
+        self.lightStatus = not self.lightStatus
+        statusLabel.config(text=self.alarmStatusStr + '\n' + self.brewStatusStr + '\n' + self.lightStatusStr)
+
+
+# Instantiate a clock
+clock = Clock()
+
+# Set up the gui
 gui = Tk()
 gui.title('SITAC')
 gui.minsize(800,600)
-
-# Alarm Strings
-currentTime = ''
-nextAlarmTime = 'Next alarm: 07:00 am'
-
-# Status Strings
-alarmStatusStr = 'Alarm: OFF'
-brewStatusStr =  'Brew:  OFF'
-lightStatusStr = 'Light: OFF'
-
-# Status Booleans (init all false)
-alarmStatus = False
-brewStatus =  False
-lightStatus = False
-
-# Fucntion that gets the current time every 200ms and updates appropriate label
-def tick():
-    global currentTime
-    # get the current local time from the PC
-    nextTime = time.strftime('%H:%M pm')
-    # if time string has changed, update it
-    if nextTime != currentTime:
-        currentTime = nextTime
-        clockLabel.config(text=nextTime)
-
-    clockLabel.after(200, tick)
-
-# Callback functions for updating gui status strings
-def updateAlarmStatus():
-    global alarmStatus, alarmStatusStr
-
-    if (alarmStatus == False):
-        alarmStatusStr = 'Alarm: ON'
-    else:
-        alarmStatusStr = 'Alarm: OFF'
-
-    alarmStatus = not alarmStatus
-    statusLabel.config(text=alarmStatusStr + '\n' + brewStatusStr + '\n' + lightStatusStr)
-
-def updateBrewStatus():
-    global brewStatus, brewStatusStr
-
-    if (brewStatus == False):
-        brewStatusStr =  'Brew:  ON'
-    else:
-        brewStatusStr =  'Brew:  OFF'
-
-    brewStatus = not brewStatus    
-    statusLabel.config(text=alarmStatusStr + '\n' + brewStatusStr + '\n' + lightStatusStr)
-
-def updateLightStatus():
-    global lightStatus, lightStatusStr
-
-    if (lightStatus == False):
-        lightStatusStr = 'Light: ON'
-    else:
-        lightStatusStr = 'Light: OFF'
-
-    lightStatus = not lightStatus
-    statusLabel.config(text=alarmStatusStr + '\n' + brewStatusStr + '\n' + lightStatusStr)
-
 
 # Set up various labels for display
 clockLabel = Label(gui, font=('times', 30, 'bold'))
 clockLabel.pack()
 
-nextAlarmLabel = Label(gui, font=('times', 15, 'bold'), text = nextAlarmTime)
+nextAlarmLabel = Label(gui, font=('times', 15, 'bold'), text = 'Next alarm: ' + clock.nextAlarmTime)
 nextAlarmLabel.pack()
 
-statusLabel = Label(gui,font=('times', 15, 'bold'),text=alarmStatusStr + '\n' + brewStatusStr + '\n' + lightStatusStr)
+statusLabel = Label(gui,font=('times', 15, 'bold'),text=clock.alarmStatusStr + '\n' + clock.brewStatusStr + '\n' + clock.lightStatusStr)
 statusLabel.pack()
 
-tick()
+
+clock.tick(clockLabel)
 gui.mainloop()
